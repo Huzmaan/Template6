5943ef6dcc3f549d7bfa0f2bdd4849ef39135b1d
Huzmaan <princehuzmaanpasta@gmail.com>
2021-11-04 08:02:11 +0500

    working

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Architects from '../Images/architect.jpg'
 
 
 const Main = () => {
     return (
         <div className="Main-css" id="Home">
         <div className="backgroundImage-css">
+            <img className="MainImg-css" src={Architects} alt="Architects" />
             <div className="MainHeading-css">
            <h1 className="Heading-css">
                <span><b className="MainHeadingText-css">BR</b></span>
